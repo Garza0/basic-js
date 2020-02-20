3a324e2a374da778f99edc44736d069a91c606c9
Garza0 <konovalovandrey7@gmail.com>
2020-02-20 22:21:54 +0200

    feat: count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,5 +1,5 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw 'Not implemented';
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let counter = 0
+  matrix.flat().forEach(el => {
+    if (el === '^^') counter++
+  })
+  return counter
 };
