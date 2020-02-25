9b1a0f6de765202d6e18b0fe42b2a423291d14f2
Garza0 <konovalovandrey7@gmail.com>
2020-02-25 13:49:59 +0200

    feat: count-cats, dream-team, recursivi-depth

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,5 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let memArr = members.filter(i => typeof i === 'string')
+  return memArr.map(i => i.toUpperCase().match(/[a-z]{1}/i)).sort().join('')
 };
