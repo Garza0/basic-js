6cca5bc42eb94ebd63297bb1d276be7de3c12d27
Garza0 <konovalovandrey7@gmail.com>
2020-03-01 16:03:24 +0200

    feat: make chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  addLink(value = '()') {
+    this.chain.push(String(value))
+    return this
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    if (position <= 0 || position % 1 || position > this.chain.length || !Number.isInteger(position)) {
+      this.chain = []
+      throw new Error()
+    }
+
+    this.chain.splice(position - 1, 1)
+    return this
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let finish = `( ${this.chain.join(' )~~( ')} )`
+    this.chain = []
+    return finish
   }
 };
 
